5d4d8ab5938acd23b4cbe195e5e4e18156d094a8
nands04 <nands04@github.com>
2026-08-30 22:18:58 -0300

    fix: build local admin

--- tina/config.ts
+++ tina/config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
 
   build: {
     outputFolder: "admin",
-    publicFolder: ".",
+    publicFolder: "",
   },
   // Uncomment to allow cross-origin requests from non-localhost origins
   // during local development (e.g. GitHub Codespaces, Gitpod, Docker).
